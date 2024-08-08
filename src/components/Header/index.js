ea4149225518c0a5c90b7e3c9ab740dd114df98f
nary406 <naryxyz3@gmail.com>
2024-08-08 21:40:13 +0530

    fifth commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -27,7 +27,7 @@ const Header = props => {
 
 
   return (
-    <nav className="main-header-container">
+    <div className="main-header-container">
       <Link to="/">
        <h1>DashBoard</h1>
       </Link>
@@ -90,7 +90,7 @@ const Header = props => {
           </button>
         </li>
       </ul>
-    </nav>
+    </div>
   )
 }
 
