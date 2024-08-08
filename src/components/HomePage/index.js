3429f65742939d39e7c9ef748be5446479e568e1
nary406 <naryxyz3@gmail.com>
2024-08-08 21:34:48 +0530

    fourth commit

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -3,7 +3,7 @@ import Header from "../Header"
 const Home=()=>{
     return<div>
         <Header/>
-       <div style={{height:"70vh", display:"flex", justifyContent:"center", alignItems:"center"}}>
+       <div style={{height:"70vh", display:"flex", flexDirection:"row",justifyContent:"center", alignItems:"center"}}>
        <h1>Welcome to Admin Panel</h1>
        </div>
     </div>
