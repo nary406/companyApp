a1c77d12a2cb31792775e160422fe35e1284e595
nary406 <naryxyz3@gmail.com>
2024-09-06 23:12:34 +0530

    seventh commit

--- src/components/AllEmployees/index.js
+++ src/components/AllEmployees/index.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Header from "../Header";
 import { IoPerson } from "react-icons/io5";
 import EditEmp from '../EditEmployee';
-import "./index.css"
+import { ThreeDots } from 'react-loader-spinner';
+import "./index.css";
 
 const AllEmp = () => {
     const [listdata, setData] = useState([]);
@@ -11,6 +12,7 @@ const AllEmp = () => {
     const [term, setTerm] = useState('');
     const [error, setError] = useState('');
     const [Edit, setEdit] = useState(false);
+    const [isLoading, setIsLoading] = useState(true); // Add loading state
 
     const caseChange = (data) => {
         const newData = data.map((item) => ({
@@ -25,6 +27,7 @@ const AllEmp = () => {
             image: item.image
         }));
         setData(newData);
+        setIsLoading(false); // Set loading state to false after data is fetched
     }
 
     useEffect(() => {
@@ -35,6 +38,7 @@ const AllEmp = () => {
             })
             .catch(err => {
                 console.log(err);
+                setIsLoading(false); // Set loading state to false in case of error
             });
     }, [Edit]);
 
@@ -68,58 +72,64 @@ const AllEmp = () => {
     const allEmployees = () => {
         return (
             <div className="table-container">
-             
-<div className="groupsearch">
-  <svg viewBox="0 0 24 24" aria-hidden="true" className="iconsearch">
-    <g>
-      <path
-        d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"
-      ></path>
-    </g>
-  </svg>
-  <input className="inputsearch" type="search" placeholder="Search"    value={searchTerm}
-                    onChange={e => setSearchTerm(e.target.value)}/>
-</div>
+                {isLoading ? (
+                    <div className="loader-container" style={{height:"100vh", display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center"}}>
+                        <ThreeDots height="60" width="60" color="blue" />
+                    </div>
+                ) : (
+                    <>
+                        <div className="groupsearch">
+                            <svg viewBox="0 0 24 24" aria-hidden="true" className="iconsearch">
+                                <g>
+                                    <path
+                                        d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"
+                                    ></path>
+                                </g>
+                            </svg>
+                            <input className="inputsearch" type="search" placeholder="Search" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} />
+                        </div>
 
-                <table className="responsive-table">
-                    <thead>
-                        <tr >
-                            <th>ID</th>
-                            <th>Image</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Mobile No</th>
-                            <th>Designation</th>
-                            <th>Gender</th>
-                            <th>Course</th>
-                            <th>Create Date</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-    {filteredData.map((item, index) => (
-        <tr key={index} >
-            <td data-label="ID">{index + 1}</td>
-            <td data-label="Image">
-                <div>
-                    {item.image === "null" ? <IoPerson /> : <img src={item.image} alt={item.name} className="profile-image" />}
-                </div>
-            </td>
-            <td data-label="Name">{item.name}</td>
-            <td data-label="Email">{item.email}</td>
-            <td data-label="Mobile No">{item.mobileno}</td>
-            <td data-label="Designation">{item.role}</td>
-            <td data-label="Gender">{item.gender}</td>
-            <td data-label="Course">{item.degree}</td>
-            <td data-label="Create Date">{item.date}</td>
-            <td data-label="Action">
-                <button id={item.Id} onClick={changePage}>Edit</button>
-                <button id={item.Id} className="delete-button" onClick={onClickDelete}>Delete</button>
-            </td>
-        </tr>
-    ))}
-</tbody>
-                </table>
+                        <table className="responsive-table">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Image</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Mobile No</th>
+                                    <th>Designation</th>
+                                    <th>Gender</th>
+                                    <th>Course</th>
+                                    <th>Create Date</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {filteredData.map((item, index) => (
+                                    <tr key={index}>
+                                        <td data-label="ID">{index + 1}</td>
+                                        <td data-label="Image">
+                                            <div>
+                                                {item.image === "null" ? <IoPerson /> : <img src={item.image} alt={item.name} className="profile-image" />}
+                                            </div>
+                                        </td>
+                                        <td data-label="Name">{item.name}</td>
+                                        <td data-label="Email">{item.email}</td>
+                                        <td data-label="Mobile No">{item.mobileno}</td>
+                                        <td data-label="Designation">{item.role}</td>
+                                        <td data-label="Gender">{item.gender}</td>
+                                        <td data-label="Course">{item.degree}</td>
+                                        <td data-label="Create Date">{item.date}</td>
+                                        <td data-label="Action">
+                                            <button id={item.Id} onClick={changePage}>Edit</button>
+                                            <button id={item.Id} className="delete-button" onClick={onClickDelete}>Delete</button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </>
+                )}
             </div>
         );
     }
